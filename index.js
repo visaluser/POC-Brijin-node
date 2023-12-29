c109adfe725fe56f347b5dbb6cebc6fc91847219
visaluser <132979509+visaluser@users.noreply.github.com>
2023-12-29 11:46:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 4000;
 app.get('/', (req, res) => {
-res.send('Hello, also Hi World!');
+res.send('Hello, also Hi World updated!');
 });
 app.listen(port, () => {
 console.log(`App listening at http://localhost:${port}`);
